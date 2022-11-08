f510ac105b3f201a28f811e2aaf2cf94f1696bd5
bombag456 <bombag456@gmail.com>
2022-11-09 03:26:45 +0600

    dq

--- dz/main.swift
+++ dz/main.swift
@@ -9,3 +9,33 @@ import Foundation
 
 print("Hello, World!")
 
+
+
+//1
+extension Array where Element:Numeric{
+    func kvadrat (arrya:Array){
+        for item in arrya{
+            print(item*item)
+        }
+    }
+}
+
+var arry = [2,4,10,20]
+    arry.kvadrat(arrya: arry)
+
+//2
+struct Searsh<T:Equatable>{
+    var arry:[T]
+    func Seash(item:T){
+        if arry.contains(item){
+            print("Элемент Найден")
+        }else{
+            print("Элемент не Найден")
+        }
+    }
+}
+var arry2 = Searsh(arry: [2,212,20,10])
+arry2.Seash(item: 212)
+
+var arr3 = Searsh(arry: ["da","no"])
+arr3.Seash(item: "Maybe")
